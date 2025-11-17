329775b36b1f645b949cbd1c4550d0c096d8c9c1
Nazar <nazarzajcev3@gmail.com>
2025-11-17 21:59:14 +0200

    Added redirect to signout

--- ChameleonPhotoredactor/Controllers/Account/Profile.cs
+++ ChameleonPhotoredactor/Controllers/Account/Profile.cs
@@ -1,10 +1,12 @@
+using BCrypt.Net;
 using ChameleonPhotoredactor.Data;
 using ChameleonPhotoredactor.Models.Entities;
 using ChameleonPhotoredactor.Models.ViewModels.Account;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
-using BCrypt.Net;
 
 namespace ChameleonPhotoredactor.Controllers
 {
@@ -15,6 +17,13 @@ namespace ChameleonPhotoredactor.Controllers
         public IActionResult Profile()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signout ()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
