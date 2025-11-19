abb18194df803b71e7fb0c2301ddf376d9623ddc
Nazar <nazarzajcev3@gmail.com>
2025-11-19 20:36:19 +0200

    Added base for Fulldit

---
+++ ChameleonPhotoredactor/Controllers/Editor/FullEditor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using ChameleonPhotoredactor.Data;
+using ChameleonPhotoredactor.Models.Entities;
+using ChameleonPhotoredactor.Models.ViewModels.Editor;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Collections.Generic;
+using System;
+using BCrypt.Net;
+using Microsoft.EntityFrameworkCore;
+
+public class FullEditorController : Controller
+{
+    [HttpGet]
+    public IActionResult FullEditor()
+    {
+        return View("~/Views/Editor/FullEditor.cshtml");
+    }
+
+}
