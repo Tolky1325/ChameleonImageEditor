f912591402aa97b768bc767d4ed905b92ef553a7
Nazar <nazarzajcev3@gmail.com>
2025-11-29 22:57:35 +0200

    Added a base for the cropping page

---
+++ ChameleonPhotoredactor/Controllers/Editor/CropImage.cs
@@ -0,0 +1,25 @@
+﻿using BCrypt.Net;
+using ChameleonPhotoredactor.Data;
+using ChameleonPhotoredactor.Models.Entities;
+using ChameleonPhotoredactor.Models.ViewModels.Editor;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+[Authorize(Roles = "User")]
+public class CropImageController : Controller
+{
+    [HttpGet]
+    public IActionResult Crop()
+    {
+        return View("~/Views/Editor/CropImage.cshtml");
+    }
+
+}
